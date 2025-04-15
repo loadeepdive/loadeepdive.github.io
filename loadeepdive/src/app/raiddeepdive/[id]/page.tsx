09910054282635202a3a92c5ee85a33de1e4a138
ljm0850 <dlwoals0850@gmail.com>
2025-04-15 22:39:47 +0900

    ldd: article separate data

--- loadeepdive/src/app/raiddeepdive/[id]/page.tsx
+++ loadeepdive/src/app/raiddeepdive/[id]/page.tsx
@@ -1,6 +1,6 @@
-// 'use client';
 import * as React from 'react';
 import fs from 'fs';
+import { markdownToMap } from '@/app/customFunction/markdownParsing';
 
 export default function RaidArticleDetail({params,}: {params: Promise<{ id: string }>;}) {
     const { id } = React.use(params);
@@ -10,7 +10,20 @@ export default function RaidArticleDetail({params,}: {params: Promise<{ id: stri
     for (const [id,title] of articleNote ){
         noteMap.set(id,title)
     }
+    const url = noteMap.has(id)? `./documents/raid/${noteMap.get(id)}`:null;
+    const file = url?markdownToMap(url):null;
+    const data = file?[...file].filter((data)=> data[0] !== 'bodyData'):null;
+    const body = file?file.get('bodyData'):'';
 
-    if (!noteMap.get(id)) return <h1>없는 글입니다.</h1>
-    return <h1>{id}번 글</h1>;
+    if (!noteMap.has(id)) return <h1>없는 글입니다.</h1>
+    return (
+    <div>
+        <h1>{id}번 글</h1>
+            <div>
+                {data?data.map((item, index)=>(<div key={index}>{item[0]} : {item[1]}</div>)):'데이터가 없습니다..'}
+            </div>
+        <h1>bodyData</h1>
+        <div>{body}</div>
+    </div>
+);
 }
