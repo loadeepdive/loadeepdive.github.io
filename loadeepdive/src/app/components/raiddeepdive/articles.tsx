1b738f571b4a422d3ec6f65b44f6f99b54a9b8a9
ljm0850 <dlwoals0850@gmail.com>
2025-04-15 16:10:01 +0900

    ldd: added function - markdownFile To Map parsing

--- loadeepdive/src/app/components/raiddeepdive/articles.tsx
+++ loadeepdive/src/app/components/raiddeepdive/articles.tsx
@@ -1,4 +1,5 @@
 import fs from 'fs/promises';
+import {markdownToMap} from '../../customFunction/markdownParsing'
 
 export default async function Articles({pathName}:{pathName:string}) {
     // 폴더 읽어오기
@@ -9,7 +10,8 @@ export default async function Articles({pathName}:{pathName:string}) {
     } catch (err) {
       console.error('폴더 읽기 실패:', err);
     }
-
+    const test = markdownToMap('./documents/raid/test.md')
+    console.log(test)
     // return
     return (
       <div>
