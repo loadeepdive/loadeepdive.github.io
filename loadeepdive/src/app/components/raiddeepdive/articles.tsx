5dc08bdee678330b27f22b4b95a0e060b8dedf7b
ljm0850 <dlwoals0850@gmail.com>
2025-04-23 21:21:22 +0900

    ldd: click articles =>  articleDetailPage

--- loadeepdive/src/app/components/raiddeepdive/articles.tsx
+++ loadeepdive/src/app/components/raiddeepdive/articles.tsx
@@ -1,5 +1,6 @@
 import fs from 'fs/promises';
-// import {markdownToMap} from '../../customFunction/markdownParsing'
+import * as fsSync from 'fs';
+import Link from 'next/link';
 
 export default async function Articles({pathName}:{pathName:string}) {
     // 폴더 읽어오기
@@ -7,22 +8,28 @@ export default async function Articles({pathName}:{pathName:string}) {
     let articleArray: string[] = [];
     try {
       articleArray = await fs.readdir(folderPath);
+      console.log(articleArray)
     } catch (err) {
       console.error('폴더 읽기 실패:', err);
     }
-    // const test = markdownToMap('./documents/raid/test.md')
-    // console.log(test)
-    // return
+    // 글과 id 매칭
+    const articleNoteFilePath = './documents/raid/articleIdNote.md'
+    const articleNote = fsSync.readFileSync(articleNoteFilePath,'utf-8').split('\n').map((data)=>data.split(':'));
+    const articleIdMap = new Map();
+    for (const [id,title] of articleNote ){articleIdMap.set(title,id)}
+
     return (
       <div>
-        <hr />
+        <hr/>
         <div>아티클스</div>
         <ul>
         {articleArray.length > 0 ? (
-          articleArray.map((article) => <li key={article}>{article} </li>)
-        ) : (
-          <li>죄송합니다. 글을 찾지 못했습니다</li>
-        )}
+          articleArray.map((article,idx) => articleIdMap.has(article)?(
+          <div key={idx}>
+            <Link href={`raiddeepdive/${articleIdMap.get(article)}`}>{article} : {articleIdMap.get(article)}</Link>
+          </div>
+        ):null
+        )) : (<li>죄송합니다. 글을 찾지 못했습니다</li>)}
       </ul>
         <hr />
       </div>
