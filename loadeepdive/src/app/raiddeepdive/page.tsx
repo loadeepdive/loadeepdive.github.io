34df3b7a171184596767516b57cc8c84650ebf44
ljm0850 <dlwoals0850@gmail.com>
2025-04-28 18:25:47 +0900

    ldd: category blind

--- loadeepdive/src/app/raiddeepdive/page.tsx
+++ loadeepdive/src/app/raiddeepdive/page.tsx
@@ -1,12 +1,12 @@
 import Bookmark from "../components/raiddeepdive/bookmark"
-import Categoty from "../components/raiddeepdive/category"
+// import Categoty from "../components/raiddeepdive/category"
 import Articles from "../components/raiddeepdive/articles"
 
 export default function raiddeepdivePage(){
     return (
         <div>
             <Bookmark></Bookmark>
-            <Categoty></Categoty>
+            {/* <Categoty></Categoty> */}
             <Articles pathName="raiddeepdive"></Articles>
         </div>
     )
